b23de3c443a69bffec439fee5d0c0ac14d50e6a5
Brian Phipps <bphipps@fanatics.com>
2017-04-16 13:16:39 -0700

    added basic go scaffolding for server

--- server/routes/status.go
+++ server/routes/status.go
@@ -1,8 +1,6 @@
 package routes
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 // Status implements a status endpoint for the api
 func Status() func(c *gin.Context) {
